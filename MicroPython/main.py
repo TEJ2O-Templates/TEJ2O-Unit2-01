6680fb7774623d9e71458fdd58231763aee5a8dc
Patrick Coxall <mr.coxall@mths.ca>
2023-08-23 02:08:08 +0000

    added python version

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,10 @@
 """
 Created by: Mr. Coxall
 Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+This module shows "Hello, MR. Coxall"
 """
 
 from microbit import *
 
 
-display.scroll("Hello, World!")
+display.scroll("Hello, Mr. Coxall!")
